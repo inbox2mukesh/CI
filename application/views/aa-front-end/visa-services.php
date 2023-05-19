373a208e9d0ba0a8dbb3aa76c83ac206576b3708
Bhupender Singh <wosa.developer2@gmail.com>
2023-05-19 17:24:09 +0530

    #Aus Frontend visa service page slider image update.

--- application/views/aa-front-end/visa-services.php
+++ application/views/aa-front-end/visa-services.php
@@ -1,5 +1,5 @@
 
-			
+
 
 
 <!--Start Services Section-->
@@ -9,42 +9,42 @@
 
 	<div class="vd-border">
 			<div class="owl-carousel-slider image-border">
-				<div class="item"><img src="<?php echo base_url('resources-f/images/slider/visa-services01.webp');?>" class="img-fullwidth"></div>
-				<div class="item"><img src="<?php echo base_url('resources-f/images/slider/visa-services02.webp');?>" class="img-fullwidth"></div>
-				<div class="item"><img src="<?php echo base_url('resources-f/images/slider/visa-services03.webp');?>" class="img-fullwidth"></div>
-				<div class="item"><img src="<?php echo base_url('resources-f/images/slider/visa-services04.webp');?>" class="img-fullwidth"></div>
+				<div class="item"><img src="<?php echo base_url('resources-f/images/slider/aus-slider-1.webp');?>" class="img-fullwidth"></div>
+				<div class="item"><img src="<?php echo base_url('resources-f/images/slider/aus-slider-2.webp');?>" class="img-fullwidth"></div>
+				<div class="item"><img src="<?php echo base_url('resources-f/images/slider/aus-slider-3.webp');?>" class="img-fullwidth"></div>
+				<!-- <div class="item"><img src="<?php echo base_url('resources-f/images/slider/visa-services04.webp');?>" class="img-fullwidth"></div> -->
 			</div>
 		</div>
 
 		<div class="services-content mt-40">
-		<h2 class="font-weight-300 text-uppercase text-center">Our 
+		<h2 class="font-weight-300 text-uppercase text-center">Our
 			<span class="text-red font-weight-600">SERVICES<a name="visa-services"></a></span>
 		</h2>
 	<!--START GRID CONTAINER -->
 				<div class="row">
-								
+
 						<!--Start Grid Items-->
 						<?php
 						foreach ($serviceData->error_message->data as $d) { ?>
 						<div class="col-md-3 col-sm-6">
-						
+
 								<a href="<?php echo base_url('visa-services/'.$d->URLslug);?>">
 									<div class="services">
 										<div class="img-area"> <img src="<?php echo base_url('uploads/service_image/'.$d->image);?>" class="img-responsive" alt="" title=""> </div>
 										<div class="img-text">
-										<h4><?php echo $d->enquiry_purpose_name;?></h4> 
-								
+										<h4><?php echo $d->enquiry_purpose_name;?></h4>
+
 										<span class="ft-btn text-center btn btn-blue btn-circled btn-sm mt-5"> Find Out More <i class="fa fa-chevron-right font-10"></i>
-										</span> 
+										</span>
 										</div>
 									</div>
 								</a>
 							</div>
-				
+
 						<?php } ?>
-						<!--End Grid Items-->	
+						<!--End Grid Items-->
 					</div>
-			
+
 				<!--END GRID CONTAINER -->
 			</div>
 		</div>
