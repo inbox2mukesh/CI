9f13da9a5a7687625f865aa8e6453b127e38b924
Mukesh <wosa.developer20@gmail.com>
2023-07-25 14:52:56 +0530

    change in url

--- application/controllers/Error_404.php
+++ application/controllers/Error_404.php
@@ -27,17 +27,14 @@ class Error_404 extends MY_Controller
     }
     function error_404_redirect()
     {
-        echo '<pre>';
-        print_r($_SERVER);
-        echo '</pre>';
-         die;
+    
+        $REDIRECT_QUERY_STRING = str_replace('/aus-staging','',$_SERVER['REDIRECT_URL']);
+        $REDIRECT_QUERY_STRING = str_replace('/aus','',$_SERVER['REDIRECT_URL']);
+
         $badurl = array('/immigration_tools/converter_a','/visa-service-details/student-extension','/visa-services/post/tourist-visa-australia','/visa-services/post/gsm-visa-australia','/visa-service-details/gsm-visa-australia','/articles/post/migration-agent-in-Melbourne-australia','/visa-services/post/partner-visa-australia','/articles/post/migration-agent-in-perth-australia','/visa-services/post/student-visa-australia','/visa-services/post/parent-visa-Australia','/visa-services/post/family-sponsored-visa-australia','/visa-services/post/citizenship-australia','/visa-services/post/employer-sponsored-visa-Australia','/why_australia','/immigration_tools/converter_b','/immigration_tools/geteducation_level/','/immigration_tools/getcandidate_age/');
-        $REDIRECT_QUERY_STRING = $_SERVER['REDIRECT_QUERY_STRING'];
         if(in_array($REDIRECT_QUERY_STRING,$badurl)){
-            echo 'gdfgd'; die;
             redirect('/');
         }
-        echo 'here'; die;
         $this->session->set_userdata('current_url',current_url());
         redirect('error_404');
     }
