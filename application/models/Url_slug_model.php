d4d6ba5b997a7db9ab13dde62707acff8e5d75ca
Mukesh <wosa.developer20@gmail.com>
2023-08-18 10:21:06 +0530

    changes in url slug model

--- application/models/Url_slug_model.php
+++ application/models/Url_slug_model.php
@@ -58,28 +58,44 @@
 			return 0;
 		}
     }
+    
     function getmetaDetails($page,$page_url)
     {
         if($page == 'articles')
         {
             $tablename = 'free_resources';
+            $this->db->select('seo_title,seo_keywords,seo_desc');
+            $this->db->from($tablename);
+            $this->db->where('URLslug',$page_url);
+            return $this->db->get()->result_array();
         }
         elseif($page == 'test-preparation-material')
         {
             $tablename = 'test_preparation_material';
+            $this->db->select('seo_title,seo_keywords,seo_desc');
+            $this->db->from($tablename);
+            $this->db->where('URLslug',$page_url);
+            return $this->db->get()->result_array();
         }
         else if($page == 'visa-services' || $page == 'online-coaching')
         {
             $tablename = 'enquiry_purpose_masters';
+            $this->db->select('seo_title,seo_keywords,seo_desc');
+            $this->db->from($tablename);
+            $this->db->where('URLslug',$page_url);
+            return $this->db->get()->result_array();
         }
         elseif($page == 'news-detail')
         {
             $tablename = 'news';
+            $this->db->select('seo_title,seo_keywords,seo_desc');
+            $this->db->from($tablename);
+            $this->db->where('URLslug',$page_url);
+            return $this->db->get()->result_array();
+        }else{
+            
         }
-        $this->db->select('seo_title,seo_keywords,seo_desc');
-        $this->db->from($tablename);
-        $this->db->where('URLslug',$page_url);
-        return $this->db->get()->result_array();
+        
     }
 
 }
