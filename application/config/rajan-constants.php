b7d13df6e0d24230614a81771e6a353c9ca60e0d
rajanbansal <wosa.developer11@gmail.com>
2023-09-01 14:24:52 +0530

    Workshop Module

--- application/config/rajan-constants.php
+++ application/config/rajan-constants.php
@@ -64,5 +64,13 @@ function is_https() {
     }
 }
 
-define("WORKSHOP_COURSES",['IELTS Academic','IELTS General Training','PTE']);
+if(DEFAULT_COUNTRY == 38) { //Canada
+    define("WORKSHOP_COURSES",['IELTS Academic','IELTS General Training','PTE','French']);
+}
+elseif(DEFAULT_COUNTRY == 13) { //Australia
+    define("WORKSHOP_COURSES",['IELTS Academic','IELTS General Training','PTE']);
+}
+else {
+    define("WORKSHOP_COURSES",['IELTS Academic','IELTS General Training','CD-IELTS Academic','CD-IELTS General Training','CELPIP General','CELPIP General LS','Duolingo','French','German','Italian','OET','PTE Academic','Spanish','Spoken English','TOEFL']);
+}
 ?>
